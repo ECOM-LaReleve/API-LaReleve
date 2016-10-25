04f620f84f6569b72ed9540715706011877a74ff
Rémi GATTAZ <remi.gattaz@gmail.com>
2016-10-25 02:04:08 +0200

    Change method of creation of our docker image
    Using an entrypoint script, it is possible to configure containers to find a mysql database

---
+++ lareleve/docker/install/mysql-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export MYSQL_HOST='mysqldb:3306'
+export MYSQL_DATABASE='lareleve'
+export MYSQL_USER='lareleve'
+export MYSQL_PASSWORD='lareleve123'
---
+++ lareleve/docker/install/wildfly-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export ACTIVATE_ADMIN=1
+export ADMIN_USERNAME='admin'
+export ADMIN_PASSWORD='admin123'
