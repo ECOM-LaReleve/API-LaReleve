52c825cc78658d1c2a012537cb43118f3123b5e8
Rémi GATTAZ <remi.gattaz@gmail.com>
2016-11-03 12:26:33 +0100

    Create PUT and DELETE calls

--- lareleve-web/src/main/java/com/lr/routes/UtilisateursRoute.java
+++ lareleve-web/src/main/java/com/lr/routes/UtilisateursRoute.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,9 +33,29 @@ public class UtilisateursRoute extends BasicRoute {
 		LOGGER.logDebug(this, "<POST>", "utilisateurEJB=[%s], utilisateur=%s",
 				(utilisateurEJB != null ? "set" : "null"), aUtilisateur);
 
-		utilisateurEJB.create(aUtilisateur);
+		try {
+			utilisateurEJB.create(aUtilisateur);
+			return Response.status(Response.Status.OK).build();
+		} catch (Exception e) {
+			LOGGER.logDebug(this, "<POST>", "Bad Request");
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+	}
 
-		return Response.status(Response.Status.OK).build();
+	@DELETE
+	@Path("{id : \\d+}") // id must be digits
+	public Response deleteUser(@PathParam("id") String id) {
+		LOGGER.logDebug(this, "<DELETE>", "utilisateurEJB=[%s], utilisateur=%s",
+				(utilisateurEJB != null ? "set" : "null"), id);
+
+		try {
+			Utilisateur utilisateur = utilisateurEJB.find(Integer.parseInt(id));
+			utilisateurEJB.remove(utilisateur);
+			return Response.status(Response.Status.OK).build();
+		} catch (Exception e) {
+			LOGGER.logDebug(this, "<DELETE>", "Bad Request");
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 	}
 
 	@GET
@@ -61,5 +83,26 @@ public class UtilisateursRoute extends BasicRoute {
 		}
 
 		return Response.status(Response.Status.NO_CONTENT).build();
+	}
+
+	@PUT
+	@Path("{id : \\d+}") // id must be digits
+	public Response updateUser(@PathParam("id") String id, Utilisateur aUtilisateur) {
+		LOGGER.logDebug(this, "<PUT>", "utilisateurEJB=[%s], utilisateur=%s",
+				(utilisateurEJB != null ? "set" : "null"), aUtilisateur);
+
+		try {
+			Utilisateur utilisateur = utilisateurEJB.find(Integer.parseInt(id));
+			utilisateur.setNom(aUtilisateur.getNom());
+			utilisateur.setPassword(aUtilisateur.getPassword());
+			utilisateur.setPrenom(aUtilisateur.getPrenom());
+			aUtilisateur.setUsername(aUtilisateur.getUsername());
+
+			utilisateurEJB.edit(utilisateur);
+			return Response.status(Response.Status.OK).build();
+		} catch (Exception e) {
+			LOGGER.logDebug(this, "<POST>", "Bad Request");
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 	}
 }
