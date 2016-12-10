853e193b742e609bd05fc946cb2268a8146bff57
Rémi GATTAZ <remi.gattaz@gmail.com>
2016-12-10 12:31:36 +0100

    Add HAWKULAR_HOST environment variable

--- lareleve/docker/install/hawkular/setup.sh
+++ lareleve/docker/install/hawkular/setup.sh
@@ -3,7 +3,7 @@
 # HAWKULAR
 if [ ! -z "$HAWKULAR_USERNAME" ]; then
     cd $INSTALL_FOLDER/hawkular/
-    curl -u $HAWKULAR_USERNAME:$HAWKULAR_PASSWORD http://192.168.99.100:8080/hawkular/wildfly-agent/download
+    curl -u $HAWKULAR_USERNAME:$HAWKULAR_PASSWORD http://$HAWKULAR_HOST/hawkular/wildfly-agent/download
     if [ $? -ne 0 ]; then
         exit 1; # Exit on failure
     fi
